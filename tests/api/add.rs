8b0ac2481deb0eface84fb77259c2bd4bc6c4219
Stefan Kroboth <stefan.kroboth@gmail.com>
2022-04-27 08:50:48 +0200

    Restructured API tests

---
+++ tests/api/add.rs
@@ -0,0 +1,153 @@
+use crate::helpers::spawn_app;
+use auditor::domain::{Component, Record, RecordTest};
+use fake::{Fake, Faker};
+
+#[tokio::test]
+async fn add_returns_a_200_for_valid_json_data() {
+    // Arange
+    let app = spawn_app().await;
+    let client = reqwest::Client::new();
+
+    // Act
+    for _ in 0..100 {
+        let body: RecordTest = Faker.fake();
+
+        let response = client
+            .post(&format!("{}/add", &app.address))
+            .header("Content-Type", "application/json")
+            .json(&body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        assert_eq!(200, response.status().as_u16());
+
+        let saved = sqlx::query_as!(
+            Record,
+            r#"SELECT
+           record_id, site_id, user_id, group_id, components as "components: Vec<Component>",
+           start_time, stop_time, runtime
+           FROM accounting
+           WHERE record_id = $1
+        "#,
+            body.record_id.as_ref().unwrap(),
+        )
+        .fetch_one(&app.db_pool)
+        .await
+        .expect("Failed to fetch data.");
+
+        assert_eq!(body, saved);
+    }
+}
+
+#[tokio::test]
+async fn add_returns_a_400_for_invalid_json_data() {
+    // Arange
+    let app = spawn_app().await;
+    let client = reqwest::Client::new();
+
+    let forbidden_strings: Vec<String> = ['/', '(', ')', '"', '<', '>', '\\', '{', '}']
+        .into_iter()
+        .map(|s| format!("test{}test", s))
+        .collect();
+
+    for field in ["record_id", "site_id", "group_id", "user_id"] {
+        for fs in forbidden_strings.iter() {
+            // Act
+            let mut body: RecordTest = Faker.fake();
+            match field {
+                "record_id" => body.record_id = Some(fs.clone()),
+                "site_id" => body.site_id = Some(fs.clone()),
+                "group_id" => body.group_id = Some(fs.clone()),
+                "user_id" => body.user_id = Some(fs.clone()),
+                _ => (),
+            }
+
+            let response = client
+                .post(&format!("{}/add", &app.address))
+                .header("Content-Type", "application/json")
+                .json(&body)
+                .send()
+                .await
+                .expect("Failed to execute request.");
+
+            assert_eq!(400, response.status().as_u16());
+
+            let saved = sqlx::query!(
+                r#"SELECT
+                   record_id, site_id, user_id, group_id,
+                   components as "components: Vec<Component>",
+                   start_time, stop_time, runtime
+                   FROM accounting
+                   WHERE record_id = $1
+                "#,
+                body.record_id.as_ref().unwrap(),
+            )
+            .fetch_all(&app.db_pool)
+            .await
+            .expect("Failed to fetch data.");
+
+            assert_eq!(saved.len(), 0);
+        }
+    }
+}
+
+#[tokio::test]
+async fn add_returns_a_400_when_data_is_missing() {
+    // Arrange
+    let app = spawn_app().await;
+    let client = reqwest::Client::new();
+
+    let record: RecordTest = Faker.fake();
+
+    let test_cases = vec![
+        ("record_id is missing", {
+            let mut r = record.clone();
+            r.record_id = None;
+            r
+        }),
+        ("site_id is missing", {
+            let mut r = record.clone();
+            r.site_id = None;
+            r
+        }),
+        ("user_id is missing", {
+            let mut r = record.clone();
+            r.user_id = None;
+            r
+        }),
+        ("group_id is missing", {
+            let mut r = record.clone();
+            r.group_id = None;
+            r
+        }),
+        ("components is missing", {
+            let mut r = record.clone();
+            r.components = None;
+            r
+        }),
+        ("start_time is missing", {
+            let mut r = record.clone();
+            r.start_time = None;
+            r
+        }),
+    ];
+
+    for (error_message, invalid_body) in test_cases {
+        // Act
+        let response = client
+            .post(&format!("{}/add", &app.address))
+            .header("Content-Type", "application/json")
+            .json(&invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_message
+        );
+    }
+}
---
+++ tests/api/helpers.rs
@@ -0,0 +1,62 @@
+use auditor::configuration::{get_configuration, DatabaseSettings};
+use auditor::telemetry::{get_subscriber, init_subscriber};
+use once_cell::sync::Lazy;
+use sqlx::{Connection, Executor, PgConnection, PgPool};
+use std::net::TcpListener;
+use uuid::Uuid;
+
+static TRACING: Lazy<()> = Lazy::new(|| {
+    let default_filter_level = "info".to_string();
+    let subscriber_name = "test".to_string();
+    if std::env::var("TEST_LOG").is_ok() {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::stdout);
+        init_subscriber(subscriber);
+    } else {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::sink);
+        init_subscriber(subscriber);
+    };
+});
+
+pub struct TestApp {
+    pub address: String,
+    pub db_pool: PgPool,
+}
+
+pub async fn spawn_app() -> TestApp {
+    Lazy::force(&TRACING);
+
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let address = format!("http://127.0.0.1:{}", port);
+
+    let mut configuration = get_configuration().expect("Failed to read configuration.");
+    configuration.database.database_name = Uuid::new_v4().to_string();
+    let connection_pool = configure_database(&configuration.database).await;
+    let server =
+        auditor::startup::run(listener, connection_pool.clone()).expect("Failed to bind address");
+    let _ = tokio::spawn(server);
+    TestApp {
+        address,
+        db_pool: connection_pool,
+    }
+}
+
+async fn configure_database(config: &DatabaseSettings) -> PgPool {
+    let mut connection = PgConnection::connect_with(&config.without_db())
+        .await
+        .expect("Failed to connect to Postgres.");
+    connection
+        .execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
+        .await
+        .expect("Failed to create database.");
+
+    // Migrate database
+    let connection_pool = PgPool::connect_with(config.with_db())
+        .await
+        .expect("Failed to connect to Postgres.");
+    sqlx::migrate!("./migrations")
+        .run(&connection_pool)
+        .await
+        .expect("Failed to migrate the database");
+    connection_pool
+}
