e2560679bcc313e0c2ce11f8ab0612303aac8b8b
Stefan Kroboth <stefan.kroboth@gmail.com>
2022-06-02 14:38:29 +0200

    Get config and epilog into container

--- scripts/test_slurm_epilog_collector.sh
+++ scripts/test_slurm_epilog_collector.sh
@@ -2,8 +2,15 @@
 set -x
 set -eo pipefail
 
+DOCKER_COMPOSE_FILE="containers/docker-centos7-slurm/docker-compose.yml"
+DOCKER_PROJECT_DIR="."
+
 function start_container() {
-	docker compose --file containers/docker-centos7-slurm/docker-compose.yml --project-directory=. up -d
+	docker compose --file $DOCKER_COMPOSE_FILE --project-directory=$DOCKER_PROJECT_DIR up -d
+	# Copy slurm.conf to container
+	docker compose --file $DOCKER_COMPOSE_FILE --project-directory=$DOCKER_PROJECT_DIR cp ./containers/docker-centos7-slurm/slurm.conf slurm:/etc/slurm/slurm.conf
+	# Copy epilog.sh to container
+	docker compose --file $DOCKER_COMPOSE_FILE --project-directory=$DOCKER_PROJECT_DIR cp ./containers/docker-centos7-slurm/epilog.sh slurm:/epilog.sh
 
 	COUNTER=0
 	until docker exec auditor-slurm-1 scontrol ping; do
@@ -13,6 +20,7 @@ function start_container() {
 			echo >&2 "Docker container did not come up in time."
 			echo >&2 "Docker logs:"
 			docker logs auditor-slurm-1
+			docker exec auditor-slurm-1 cat /var/log/slurm/slurmctld.log
 			stop_container
 			echo >&2 "Exiting."
 			exit 1
@@ -23,19 +31,19 @@ function start_container() {
 
 function stop_container() {
 	echo >&2 "Stopping container"
-	docker compose --file containers/docker-centos7-slurm/docker-compose.yml --project-directory=. down
+	docker compose --file $DOCKER_COMPOSE_FILE --project-directory=$DOCKER_PROJECT_DIR down
 }
 
 function start_auditor() {
 	./target/debug/auditor &
-	SERVER_PID=$!
+	AUDITOR_SERVER_PID=$!
 	COUNTER=0
 	until curl http://localhost:8000/health_check; do
-		>&2 echo "auditor is still unavailable - sleeping"
+		>&2 echo "Auditor is still unavailable - sleeping"
 		let COUNTER=COUNTER+1
 		if [ "$COUNTER" -gt "30" ]; then
 			echo >&2 "Auditor did not come up in time."
-			stop_auditor $SERVER_PID
+			stop_auditor $AUDITOR_SERVER_PID
 			echo >&2 "Exiting."
 			exit 1
 		fi
@@ -45,7 +53,7 @@ function start_auditor() {
 
 function stop_auditor() {
 	echo >&2 "Stopping Auditor"
-	kill $1
+	kill $AUDITOR_SERVER_PID
 }
 
 
@@ -57,5 +65,5 @@ start_auditor
 docker exec auditor-slurm-1 sinfo
 
 stop_container
-stop_auditor $SERVER_PID
+stop_auditor
 
