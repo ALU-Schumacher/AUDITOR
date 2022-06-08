de7f8463d96d9fe40d26d7e78cf9024dfdfcffb7
Stefan Kroboth <stefan.kroboth@gmail.com>
2022-06-08 11:58:22 +0200

    Create randomly named database for each test run

--- src/collectors/slurm_epilog/main.rs
+++ src/collectors/slurm_epilog/main.rs
@@ -89,45 +89,5 @@ async fn main() -> Result<(), Error> {
 
     client.add(record).await?;
 
-    //  let output: String = r#"JobId=19771 JobName=arc_pilot
-    // UserId=atlpr001(301501) GroupId=atlpr(300001) MCS_label=N/A
-    // Priority=59971 Nice=50 Account=atlpr QOS=normal
-    // JobState=RUNNING Reason=None Dependency=(null)
-    // Requeue=0 Restarts=0 BatchFlag=1 Reboot=0 ExitCode=0:0
-    // RunTime=00:15:04 TimeLimit=15:00:00 TimeMin=N/A
-    // SubmitTime=2022-05-04T13:56:09 EligibleTime=2022-05-04T13:56:09
-    // AccrueTime=2022-05-04T13:56:09
-    // StartTime=2022-05-04T13:56:10 EndTime=2022-05-05T04:56:10 Deadline=N/A
-    // SuspendTime=None SecsPreSuspend=0 LastSchedEval=2022-05-04T13:56:10 Scheduler=Main
-    // Partition=grid_medium_mcore AllocNode:Sid=arc3:1007
-    // ReqNodeList=(null) ExcNodeList=(null)
-    // NodeList=host-10-18-1-5
-    // BatchHost=host-10-18-1-5
-    // NumNodes=1 NumCPUs=8 NumTasks=8 CPUs/Task=1 ReqB:S:C:T=0:0:*:*
-    // TRES=cpu=8,mem=16000M,node=1,billing=8
-    // Socks/Node=* NtasksPerN:B:S:C=8:0:*:* CoreSpec=*
-    // MinCPUsNode=8 MinMemoryCPU=2000M MinTmpDiskNode=0
-    // Features=(null) DelayBoot=00:00:00
-    // OverSubscribe=OK Contiguous=0 Licenses=(null) Network=(null)
-    // Command=/tmp/SLURM_job_script.wdil52
-    // WorkDir=/pool_home/arc6/session/FhRKDmoWS60nQ0j3QqGs20FqABFKDmABFKDmEYKKDmMBFKDmbGuhXn
-    // StdErr=/pool_home/arc6/session/FhRKDmoWS60nQ0j3QqGs20FqABFKDmABFKDmEYKKDmMBFKDmbGuhXn.comment
-    // StdIn=/dev/null
-    // StdOut=/pool_home/arc6/session/FhRKDmoWS60nQ0j3QqGs20FqABFKDmABFKDmEYKKDmMBFKDmbGuhXn.comment
-    // Power="#
-    //      .to_string();
-    //
-    //  println!("{}", output);
-    //
-    //  let blah: HashMap<&str, &str> = output
-    //      .split_whitespace()
-    //      .map(|s| {
-    //          let t = s.split("=").take(2).collect::<Vec<_>>();
-    //          (t[0], t[1])
-    //      })
-    //      .collect();
-    //
-    //  println!("{:#?}", blah);
-
     Ok(())
 }
