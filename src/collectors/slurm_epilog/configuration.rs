a826c6590bed435f2f55e288499c6f390851357a
Stefan Kroboth <stefan.kroboth@gmail.com>
2022-06-08 17:10:29 +0200

    Updated dependencies

--- src/collectors/slurm_epilog/configuration.rs
+++ src/collectors/slurm_epilog/configuration.rs
@@ -32,24 +32,31 @@ fn default_string() -> String {
 /// Loads the configuration from a file `configuration.{yaml,json,toml,...}`
 #[tracing::instrument(name = "Loading configuration")]
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
-    // initialize our configuration reader
-    let mut settings = config::Config::default();
-
     let base_path = std::env::current_dir().expect("Failed to determine the current directory");
     let configuration_directory = base_path
         .join("configuration")
         .join("slurm-epilog-collector");
 
-    settings.merge(config::File::from(configuration_directory.join("base")).required(false))?;
+    let settings = config::Config::builder()
+        .add_source(config::File::from(configuration_directory.join("base")).required(true));
+    let settings = match std::env::args().nth(1) {
+        Some(file) => settings.add_source(config::File::from(file.as_ref()).required(false)),
+        None => settings,
+    };
+    let settings = settings.add_source(config::Environment::with_prefix("auditor").separator("__"));
+    // settings.merge(config::File::from(configuration_directory.join("base")).required(false))?;
 
-    match std::env::args().nth(1) {
-        Some(file) => {
-            settings.merge(config::File::from(file.as_ref()).required(false))?;
-        }
-        None => (),
+    // match std::env::args().nth(1) {
+    //     Some(file) => {
+    //         settings.merge(config::File::from(file.as_ref()).required(false))?;
+    //     }
+    //     None => (),
+    // }
+
+    // settings.merge(config::Environment::with_prefix("auditor").separator("_"))?;
+
+    match settings.build() {
+        Ok(config) => config.try_deserialize(),
+        Err(e) => Err(e),
     }
-
-    settings.merge(config::Environment::with_prefix("auditor").separator("_"))?;
-
-    settings.try_into()
 }
