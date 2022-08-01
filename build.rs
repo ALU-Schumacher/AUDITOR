1f7024696c0bd35489e56f074a7a5a2956268a28
Stefan Kroboth <stefan.kroboth@gmail.com>
2022-08-01 10:23:08 +0200

    Fix building docs on docs.rs

---
+++ build.rs
@@ -0,0 +1,13 @@
+// Copyright 2021-2022 AUDITOR developers
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+// http://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
+
+fn main() {
+    // When building in docs.rs, we want to set SQLX_OFFLINE mode to true
+    if std::env::var_os("DOCS_RS").is_some() {
+        println!("cargo:rustc-env=SQLX_OFFLINE=true");
+    }
+}
