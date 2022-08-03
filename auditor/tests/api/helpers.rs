890a4b78bc5ba4916be5e1776e6ce58832a58b15
Stefan Kroboth <stefan.kroboth@gmail.com>
2022-08-03 13:56:45 +0200

    Introduced workspaces

--- auditor/tests/api/helpers.rs
+++ auditor/tests/api/helpers.rs
@@ -54,7 +54,7 @@ async fn configure_database(config: &DatabaseSettings) -> PgPool {
     let connection_pool = PgPool::connect_with(config.with_db())
         .await
         .expect("Failed to connect to Postgres.");
-    sqlx::migrate!("./migrations")
+    sqlx::migrate!("./../migrations")
         .run(&connection_pool)
         .await
         .expect("Failed to migrate the database");
