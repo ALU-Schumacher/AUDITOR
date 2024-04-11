cd63cf510a9345b24c0149f34dbe28d4231d3853
raghuvar-vijay <raghuvarvijayakumar@gmail.com>
2024-04-11 15:06:55 +0200

    refactor: Format and fix benchmark scripts and add README

--- auditor/benches/configuration.rs
+++ auditor/benches/configuration.rs
@@ -1,4 +1,3 @@
-
 use serde_aux::field_attributes::deserialize_number_from_string;
 
 #[derive(serde::Deserialize, Debug)]
@@ -17,31 +16,11 @@ pub fn get_configuration() -> Result<AuditorSettings, config::ConfigError> {
     let base_path = std::env::current_dir().expect("Failed to determine the current directory");
     let configuration_directory = base_path.join("benches").join("configuration");
 
-
     let base_yaml_path = configuration_directory.join("bench.yaml");
-    
 
     let mut settings = config::Config::builder();
-    //settings = config::Config::builder()         
-    //    .add_source(config::File::from(configuration_directory.join("bench")).required(true),);
-        
+
     settings = settings.add_source(config::File::from(base_yaml_path).required(true));
-
-    //let settings = match std::env::args().nth(1) {
-    //    Some(file) => settings.add_source(
-    //        config::File::from(file.as_ref())
-    //            .required(false)
-    //            .format(config::FileFormat::Yaml),
-    //    ),
-    //    None => settings,
-    //};
-
-    //let settings = settings.add_source(
-    //    config::Environment::with_prefix("AUDITOR")
-    //        .separator("__")
-    //        .prefix_separator("_"),
-    //);
 
     settings.build()?.try_deserialize()
 }
-
