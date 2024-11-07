7719b7f440e91ae6b48390013e5d1a83012d8149
Max Kühn <maxfischer2781@gmail.com>
2024-11-07 10:39:10 +0100

    add mode, correct typing

--- plugins/apel/src/auditor_apel_plugin/utility.py
+++ plugins/apel/src/auditor_apel_plugin/utility.py
@@ -1,15 +1,27 @@
 import os
 import pathlib
 from contextlib import contextmanager
-from typing import Generator, TextIO
+from typing import Literal, overload, IO, ContextManager
+
+
+@overload
+def write_transaction(
+    path: str | os.PathLike[str], mode: Literal["w"] = ..., **kwargs
+) -> ContextManager[IO[str]]: ...
+
+
+@overload
+def write_transaction(
+    path: str | os.PathLike[str], mode: Literal["wb"], **kwargs
+) -> ContextManager[IO[bytes]]: ...
 
 
 @contextmanager
 def write_transaction(
-    path: str | os.PathLike[str], **kwargs
-) -> Generator[TextIO, None, None]:
+    path: str | os.PathLike[str], mode: Literal["w", "wb"] = "w", **kwargs
+):
     """Open `path` for overwriting but discard the new content if an exception occurs"""
     tmp_path = pathlib.Path(path).parent / f".{pathlib.Path(path).name}.tmp"
-    with open(tmp_path, "w", **kwargs) as out_stream:
+    with open(tmp_path, mode, **kwargs) as out_stream:
         yield out_stream
     tmp_path.rename(path)
