9ab0199209114976d2caa62bac4b5541f08a78e6
Max Kühn <maxfischer2781@gmail.com>
2024-11-07 16:17:24 +0100

    restrict typing to cover pre-3.10

--- plugins/apel/src/auditor_apel_plugin/utility.py
+++ plugins/apel/src/auditor_apel_plugin/utility.py
@@ -1,4 +1,3 @@
-import os
 import pathlib
 from contextlib import contextmanager
 from typing import IO, ContextManager, Literal, overload
@@ -6,19 +5,19 @@ from typing import IO, ContextManager, Literal, overload
 
 @overload
 def write_transaction(
-    path: "str | os.PathLike[str]", mode: Literal["w"] = ..., **kwargs
+    path: "str | pathlib.PurePath", mode: Literal["w"] = ..., **kwargs
 ) -> ContextManager[IO[str]]: ...
 
 
 @overload
 def write_transaction(
-    path: "str | os.PathLike[str]", mode: Literal["wb"], **kwargs
+    path: "str | pathlib.PurePath", mode: Literal["wb"], **kwargs
 ) -> ContextManager[IO[bytes]]: ...
 
 
 @contextmanager
 def write_transaction(
-    path: "str | os.PathLike[str]", mode: Literal["w", "wb"] = "w", **kwargs
+    path: "str | pathlib.PurePath", mode: Literal["w", "wb"] = "w", **kwargs
 ):
     """Open `path` for overwriting but discard the new content if an exception occurs"""
     tmp_path = pathlib.Path(path).parent / f".{pathlib.Path(path).name}.tmp"
