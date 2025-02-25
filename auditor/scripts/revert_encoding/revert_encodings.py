d6dd27175f7ef2cb9eb2b5f5730a762b1205fee1
raghuvar-vijay <raghuvarvijayakumar@gmail.com>
2025-02-25 14:25:10 +0100

    refactor: remove default values for db config & add requests library to requirements.txt

--- auditor/scripts/revert_encoding/revert_encodings.py
+++ auditor/scripts/revert_encoding/revert_encodings.py
@@ -6,15 +6,15 @@ from urllib.parse import unquote
 import psycopg2
 from dotenv import load_dotenv
 
-load_dotenv(dotenv_path="auditor/scripts/revert_encoding")
+load_dotenv(dotenv_path="auditor/scripts/revert_encoding/.env")
 
 
 DB_CONFIG = {
-    "dbname": os.getenv("DB_NAME", "auditor"),
-    "user": os.getenv("DB_USER", "postgres"),
-    "password": os.getenv("DB_PASSWORD", "password"),
-    "host": os.getenv("DB_HOST", "localhost"),
-    "port": os.getenv("DB_PORT", "5432"),
+    "dbname": os.getenv("DB_NAME"),
+    "user": os.getenv("DB_USER"),
+    "password": os.getenv("DB_PASSWORD"),
+    "host": os.getenv("DB_HOST"),
+    "port": os.getenv("DB_PORT"),
 }
 
 
