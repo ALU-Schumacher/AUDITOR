44f78a51d0b0213c451f7924cb6b6bcbb52e7f53
raghuvar-vijay <raghuvarvijayakumar@gmail.com>
2026-05-14 21:06:18 +0200

    Add script to migrate millisec to sec for slurm totalcpu field

---
+++ auditor/scripts/convert_slurm_millisec_to_sec/test_convert_slurm_millisec_to_sec.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+
+import asyncio
+from pyauditor import AuditorClientBuilder
+
+
+async def main():
+
+    client = AuditorClientBuilder().address("127.0.0.1", 8000).timeout(10).build()
+
+    records = await client.get()
+    assert len(records) == 2
+
+    for record in records:
+
+        list_of_components = list(record.components)
+
+        assert list_of_components[0].name == "TotalCPU"
+
+        assert list_of_components[0].amount == 0
+
+        assert list_of_components[1].name == "TotalCPU_milli"
+
+        assert list_of_components[1].amount == 273
+
+
+if __name__ == "__main__":
+    import time
+
+    s = time.perf_counter()
+    asyncio.run(main())
